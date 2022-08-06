514d3809f2b02334586d2926f5defeed1b1c85ba
Arda Basaran <ardarbasaran@gmail.com>
2022-08-06 15:55:27 -0400

    Add initial UI elements

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+import { Box, CssBaseline, Grid, Paper } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import FallingStars from './FallingStars'
+import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
+const darkTheme = createTheme({ palette: { mode: 'dark' } });
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <FallingStars />
+        </ThemeProvider>
+    </React.StrictMode>
 )
